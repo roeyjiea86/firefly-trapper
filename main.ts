051ac70f04564c2537ecde0a7816a094a7b7dac7
roeyjiea86 <166978203+roeyjiea86@users.noreply.github.com>
2025-09-06 14:06:47 +0800

    add yellowLit and greenLit

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 let maleLights = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 let femaleLights = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
+let score = 0
+let yellowLit = false
+let greenLit = false
 basic.forever(function () {
 	
 })
