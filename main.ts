aaff728563a4c313fae67bed37125936e4236df2
roeyjiea86 <166978203+roeyjiea86@users.noreply.github.com>
2025-09-06 20:21:08 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,20 @@ let femaleLights = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
 let score = 0
 let yellowLit = false
 let greenLit = false
+loops.everyInterval(10000, function () {
+    maleLights.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    yellowLit = true
+    basic.pause(1000)
+    maleLights.showColor(neopixel.colors(NeoPixelColors.Black))
+    yellowLit = false
+})
 basic.forever(function () {
 	
+})
+loops.everyInterval(randint(0, 10000), function () {
+    femaleLights.showColor(neopixel.colors(NeoPixelColors.Green))
+    greenLit = true
+    basic.pause(1000)
+    femaleLights.showColor(neopixel.colors(NeoPixelColors.Black))
+    greenLit = false
 })
